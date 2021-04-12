92e760104e5e19983512febc3e2939f6b63aef35
AntonioAiltonGoncalves <antonioailton723@gmail.com>
2021-04-12 08:06:39 -0300

    finalizado até 1020

--- Exercicios/ExerciciosURI/iniciante/1018.js
+++ Exercicios/ExerciciosURI/iniciante/1018.js
@@ -1,10 +1,12 @@
 `mode strict`
 
 const lines = prompt("Digite seu problema: ");
-let valor = lines.map(Number);
-const [um,dois,tres,quatro,cinco,seis,set] = [100,50,20,10,5,2,1];
-
-// function testeNota(nota,valorTotal){
-//     if((valorTotal%nota) = 0){   
-//     }
-// }
+let valor = parseInt(lines);
+const valores = [100,50,20,10,5,2,1];
+console.log(valor);
+for(let nota of valores)
+{
+let quantidade = parseInt(valor/nota);
+console.log(`${quantidade} nota(s) de R$ ${nota},00`);
+valor = valor%nota;
+};
